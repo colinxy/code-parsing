78c4c1eee0ff29629dcf08cc469a5dd35aefa273
Xinyu Yang <colin.yangxinyu@outlook.com>
2015-12-10 13:58:10 -0800

    add more regex

--- cpp_segment.py
+++ cpp_segment.py
@@ -1,7 +1,14 @@
 
 """
-Component of function paattern matching
+Component of function pattern matching
 """
+
+# left curly brace of the function definition
+# goes on the end of line, or on a new line
+END_OF_LINE = 0
+NEW_LINE    = 1
+
+STYLE       = END_OF_LINE
 
 
 class Type:
@@ -10,28 +17,35 @@ class Type:
         self.major_type = ''
         self.modifier = ''
         self.pointer_ref = ''
+    
+    def parse_type(self):
+        pass
 
 
 class Func:
-    # left curly brace of function definition
-    # goes on the end of line, or on a new line
-    END_OF_LINE = 0
-    NEW_LINE    = 1
     def __init__(self, rtn_type, func_name, arg_str, func_body):
         self.rtn_type  = Type(rtn_type)
         self.func_name = func_name
         self.arg_str   = arg_str
         self.func_body = func_body
 
-    def __str__(self, style=END_OF_LINE):
+    def __str__(self):
         code_format = (
             "{} {}({})%s\{\n"
             "    {}\n"
             "\}"
-        ) % (' ' if style == END_OF_LINE else '\n')
-        
+        ) % (' ' if STYLE == END_OF_LINE else '\n')
+
         code_str = code_format.format(str(self.rtn_type),
-                                  func_name,
-                                  arg_str,
-                                  func_body)
+                                      func_name,
+                                      arg_str,
+                                      func_body)
         return code_str
+
+    def jsonify(self):
+        pass
+
+
+class Class:
+    def __init__(self):
+        pass
---
+++ parse_cpp.py
@@ -0,0 +1,124 @@
+#! /usr/bin/env python3
+
+"""
+A parser for C++ programming language
+
+This parser generates statistical infomation about the program,
+and offers code formatting functionalities.
+This parser only implements a subset of the C++ programming language,
+and should not be relied on as a full-fledged code analyzer.
+
+Algorithm:
+The program is based on regular expression to
+parse general infomation of a C++ source file.
+It cannot be used to check for syntax error.
+If there is syntax error in the C++ source file,
+the parsed result is likely to be erroneous
+
+Match all the top level include header, global variable,
+function definition, class definition
+
+NOTICE: This module assumes many coding conventions,
+and should not be used as a reliable code analyzer.
+The following coding conventions were assumed:
+
+- All the headers files are put at the top of the file
+- No parentheses in function perimeter list
+"""
+
+import re
+import sys
+
+
+# fundamantal types
+FUND_TYPES = [
+    'void',
+    'bool',
+    'char',
+    'short',
+    'int',
+    'long',
+    'long long',
+    'float',
+    'double'
+]
+
+MODIFIERS = [
+    'signed',
+    'unsigned',
+    'short',
+    'long',
+    'const',
+    'volatile',
+    'restrict',
+    'extern'
+]
+
+
+# optional modifier
+MODIFIER = '(?:(' + '|'.join(MODIFIERS) + ')\s+)?'
+
+# type name + possible pointer type + possible reference type
+TYPE = r'([a-zA-Z_][\w_]*\s*\**\s*&?)'
+
+# identifier
+IDENTIFIER = r'([a-zA-Z_][\w_]*)'
+
+# pattern of variable/function declaration(definition)
+# capture modifier, type, identifier
+VAR_DEF = re.compile(
+    ''.join([
+        MODIFIER,               # 1 modifier supported
+        TYPE + r'\s*',          # match type infomation
+        IDENTIFIER,             # identifier
+        '.*;'                   # initialization, ended by semicolon
+    ])
+    , re.X | re.S
+)
+
+# pattern of function definition
+# capture return type, function name, argument list, function body
+FUNC_DEF = re.compile(
+    ''.join([
+        MODIFIER,               # 1 modifier supported
+        TYPE + r'\s*',          # match type infomation
+        IDENTIFIER              # identifier
+        r'\s*\(',               # left parenthese of argument list
+        r'(.*)',                # capture argument list
+        r'\)\s*',               # left parenthese of argument list
+        r'\{'
+    ])
+    , re.X | re.S
+)
+
+# pattern of class definition
+# capture member variables
+CLASS_DEF = re.compile(
+    ''.join([
+        r'(class|struct)\s+',
+        IDENTIFIER
+    ])
+    , re.X | re.S
+)
+
+COMMENT = re.compile(r'/\*.*\*/', re.S)
+
+# pattern of include header
+# capture the name of the header, user header / system header
+INCLUDE_HEADER = re.compile(
+    r"""\#include\s*    # include directive
+        (\"|<)(.+)(\"|>)"""
+    , re.X
+)
+
+FUNC  = IDENTIFIER + r'\s*' + r'\(([^\(\)])\)\s*\{\}'
+MAIN  = r'(int|void)\s+(main)\W'
+CLASS = r'\s+(class|struct)\W'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
